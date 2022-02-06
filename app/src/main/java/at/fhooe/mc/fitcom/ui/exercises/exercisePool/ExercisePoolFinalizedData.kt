58ec938927db4bb75dd09c005a5702b73f8c0b15
Sebastian Weidlinger <s.weidlinger99@gmx.at>
2022-02-06 17:07:09 +0100

    Added Finalized Exercise List

---
+++ app/src/main/java/at/fhooe/mc/fitcom/ui/exercises/exercisePool/ExercisePoolFinalizedData.kt
@@ -0,0 +1,8 @@
+package at.fhooe.mc.fitcom.ui.exercises.exercisePool
+
+data class ExercisePoolFinalizedData(
+    var id: Int,
+    var name: String,
+    var category: Int,
+    var image: String
+)
